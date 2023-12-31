ecc14ca589bc3d3c82c91ad86f05b6f185ec920f
NTVui <soidee2212@gmail.com>
2023-12-31 23:25:25 +0700

    Add thuc_hanh1.cpp file

---
+++ thuc_hanh1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //B
+    cout << "Hello World!" << endl;
+    int a = 5;
+    int b = 6;
+    int tong = a + b;
+    cout<<"Tong = "<<tong<<endl;
+
+    system("pause");
+}
